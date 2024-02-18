b53aec23d56a4afa0d0ae04676258a2804eaa371
Nélio <jrneliodias@gmail.com>
2024-02-18 15:17:39 -0300

    feat: add PostgresCreateUserRepository

--- src/repositories/postgres/create-user.js
+++ src/repositories/postgres/create-user.js
@@ -1,6 +1,6 @@
 import { PostgresHelper } from '../../db/postgres/helper'
 
-export class PostgresCreateUserRepositor {
+export class PostgresCreateUserRepository {
     async execute(createUserParams) {
         const results = await PostgresHelper.query(
             'INSERT INTO USERS (ID,first_name,last_name,email,password) VALUES ($1,$2,$3,$4,$5)',
