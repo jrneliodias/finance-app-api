c6c98572dc5a2368ab902442b39bd1ad02f2fdaa
Nélio <jrneliodias@gmail.com>
2024-02-21 23:30:24 -0300

    feat: add PostgresDeleteUserRepository

---
+++ src/repositories/postgres/delete-user.js
@@ -0,0 +1,13 @@
+import { PostgresHelper } from '../../db/postgres/helper'
+
+export class PostgresDeleteUserRepository {
+    async execute(userId) {
+        const deletedUser = await PostgresHelper.query(
+            `DELETE FROM users 
+             WHERE id = $1 
+             RETURNING *`,
+            [userId],
+        )
+        return deletedUser[0]
+    }
+}
