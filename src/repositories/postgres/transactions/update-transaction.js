ed8ee9058768427c79b12111194901e331cda39b
Nélio <jrneliodias@gmail.com>
2024-02-27 20:08:46 -0300

    fix: PostgresHelper import path

--- src/repositories/postgres/transactions/update-transaction.js
+++ src/repositories/postgres/transactions/update-transaction.js
@@ -1,4 +1,4 @@
-import { PostgresHelper } from '../../../db/postgres/helper'
+import { PostgresHelper } from '../../../db/postgres/helper.js'
 
 export class PostgresUpdateTransactionRepository {
     async execute(userId, updateTransactionParams) {
