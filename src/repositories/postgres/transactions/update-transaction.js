6fede7cd1434f5c3915fa78e0d4d56463959f926
Nélio <jrneliodias@gmail.com>
2024-02-27 19:12:55 -0300

    feat: create PostgresTransactionParamsRepository

---
+++ src/repositories/postgres/transactions/update-transaction.js
@@ -0,0 +1,31 @@
+import { PostgresHelper } from '../../../db/postgres/helper'
+
+export class PosgresUpdateTransactionParamsRepository {
+    async execute(userId, updateTransactionParams) {
+        try {
+            const updateFields = []
+            const updateValues = []
+
+            Object.keys(updateTransactionParams).forEach((key) => {
+                updateFields.push(`${key} = $${updateValues.length + 1}`)
+                updateValues.push(updateTransactionParams[key])
+            })
+
+            updateValues.push(userId)
+
+            const updateQuery = `
+        UPDATE transactions
+        SET ${updateFields.join(', ')}
+        WHERE id = $${updateValues.length}
+        RETURNING *
+        `
+            const updateTransaction = await PostgresHelper.query(
+                updateQuery,
+                updateValues,
+            )
+            return updateTransaction[0]
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
