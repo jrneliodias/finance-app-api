29a20220f2b33bf5cea4ca50f6a9ef7cbf9302c8
Nélio <jrneliodias@gmail.com>
2024-02-27 22:33:18 -0300

    feat: create DeleteTransactionUseController

---
+++ src/controllers/transactions/delete-transaction.js
@@ -0,0 +1,34 @@
+import validator from 'validator'
+import {
+    notFound,
+    ok,
+    serverError,
+    invalidIdResponse,
+} from '../helpers/index.js'
+
+export class DeleteTransactionController {
+    constructor(DeleteTransactionUseCase) {
+        this.deleteTransactionUseCase = DeleteTransactionUseCase
+    }
+    async execute(httpRequest) {
+        try {
+            const transactionId = httpRequest.params.TransactionId
+            const isIdValid = validator.isUUID(transactionId)
+
+            if (!isIdValid) {
+                return invalidIdResponse()
+            }
+
+            const deletedTransaction =
+                await this.deleteTransactionUseCase.execute(transactionId)
+
+            if (!deletedTransaction) {
+                return notFound()
+            }
+            return ok(deletedTransaction)
+        } catch (error) {
+            console.error(error)
+            return serverError()
+        }
+    }
+}
--- src/controllers/transactions/index.js
+++ src/controllers/transactions/index.js
@@ -1,3 +1,4 @@
 export * from './create-transaction.js'
+export * from './delete-transaction.js'
 export * from './get-transaction-by-user-id.js'
 export * from './update-transaction.js'
