b1965f2c0edb12365ac4aae4e6a3c2159a0e6432
Nélio <jrneliodias@gmail.com>
2024-02-29 15:31:01 -0300

    feat: add PostgresGetUserRepository

--- src/repositories/postgres/users/index.js
+++ src/repositories/postgres/users/index.js
@@ -1,5 +1,6 @@
 export * from './create-user.js'
 export * from './delete-user.js'
 export * from './get-user-by-id.js'
+export * from './get-user-balance.js'
 export * from './get-user-by-email.js'
 export * from './update-user.js'
