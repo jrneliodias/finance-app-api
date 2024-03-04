9fbacf62f44a47743bf6ec0a353a431394977270
Nélio <jrneliodias@gmail.com>
2024-03-04 09:54:27 -0300

    feat: add types/jest

--- src/controllers/users/create-user.test.js
+++ src/controllers/users/create-user.test.js
@@ -27,4 +27,43 @@ describe('Create User Controller', () => {
         expect(result.statusCode).toBe(201)
         expect(result.body).not.toBeUndefined()
     })
+    it('should return 400 if first_name is not provided', async () => {
+        const createUserUseCase = new CreateUserCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        // act
+        const httpRequest = {
+            body: {
+                last_name: 'Dias',
+                email: 'nelio@mail.com',
+                password: '123456',
+            },
+        }
+
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body).not.toBeUndefined()
+    })
+
+    it('should return 400 if password is less tha chacteres', async () => {
+        const createUserUseCase = new CreateUserCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        // act
+        const httpRequest = {
+            body: {
+                last_name: 'Dias',
+                email: 'nelio@mail.com',
+                password: '123',
+            },
+        }
+
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body).not.toBeUndefined()
+    })
 })
