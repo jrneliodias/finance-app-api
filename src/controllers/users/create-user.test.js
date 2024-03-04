48c66b0c1655acf0435827ea7e29da90d43ae1f8
Nélio <jrneliodias@gmail.com>
2024-03-04 11:32:09 -0300

    test: ensure tha CreateUserUseCase throws 400 if CreateUserUseCase throws EmailAlreadyInUseError

--- src/controllers/users/create-user.test.js
+++ src/controllers/users/create-user.test.js
@@ -1,4 +1,6 @@
+import { EmailAlreadyInUseError } from '../../errors/user.js'
 import { CreateUserController } from './create-user.js'
+import { faker } from '@faker-js/faker'
 
 describe('Create User Controller', () => {
     class CreateUserCaseStub {
@@ -14,10 +16,10 @@ describe('Create User Controller', () => {
         // act
         const httpRequest = {
             body: {
-                first_name: 'Nelio',
-                last_name: 'Dias',
-                email: 'nelio@mail.com',
-                password: '123456',
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({ length: 10 }),
             },
         }
 
@@ -34,9 +36,9 @@ describe('Create User Controller', () => {
         // act
         const httpRequest = {
             body: {
-                last_name: 'Dias',
-                email: 'nelio@mail.com',
-                password: '123456',
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({ length: 10 }),
             },
         }
 
@@ -54,9 +56,9 @@ describe('Create User Controller', () => {
         // act
         const httpRequest = {
             body: {
-                last_name: 'Dias',
-                email: 'nelio@mail.com',
-                password: '123',
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({ length: 10 }),
             },
         }
 
@@ -74,10 +76,10 @@ describe('Create User Controller', () => {
         // act
         const httpRequest = {
             body: {
-                first_name: 'Nélio',
-                last_name: 'Dias',
-                email: 'nelio@mail.com',
-                password: '123',
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({ length: 10 }),
             },
         }
         const executeSpy = jest.spyOn(createUserUseCase, 'execute')
@@ -87,5 +89,51 @@ describe('Create User Controller', () => {
         // assert
         expect(executeSpy).toHaveBeenCalledWith(httpRequest.body)
         expect(executeSpy).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return 500 if CreateUseCase throws', async () => {
+        const createUserUseCase = new CreateUserCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        // act
+        const httpRequest = {
+            body: {
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({ length: 10 }),
+            },
+        }
+        jest.spyOn(createUserUseCase, 'execute').mockImplementationOnce(() => {
+            throw new Error()
+        })
+
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(500)
+    })
+
+    it('should return 300 if CreateUseCase throws EmailAlreadyUseError', async () => {
+        const createUserUseCase = new CreateUserCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        // act
+        const httpRequest = {
+            body: {
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({ length: 10 }),
+            },
+        }
+        jest.spyOn(createUserUseCase, 'execute').mockImplementationOnce(() => {
+            throw new EmailAlreadyInUseError(httpRequest.body.email)
+        })
+
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
     })
 })
