b7d60dde28dad73d7770058b75ac1187468719d8
Nélio <jrneliodias@gmail.com>
2024-03-06 21:35:11 -0300

    test: ensure GetUserBalanceController return 200 when getting user balance

---
+++ src/controllers/users/get-user-balance.test.js
@@ -0,0 +1,69 @@
+import { faker } from '@faker-js/faker'
+import { GetUserBalanceController } from './get-user-balance.js'
+
+describe('GetUserBalanceController', () => {
+    class GetUserBalanceUseCaseStub {
+        execute() {
+            return faker.number.int()
+        }
+    }
+
+    const makeSut = () => {
+        const getUserBalanceUseCase = new GetUserBalanceUseCaseStub()
+        const sut = new GetUserBalanceController(getUserBalanceUseCase)
+        return { getUserBalanceUseCase, sut }
+    }
+
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+    }
+
+    it('should return 200 when getting user balance', async () => {
+        const { sut } = makeSut()
+
+        // act
+        const httpResponse = await sut.execute(httpRequest)
+
+        // assert
+        expect(httpResponse.statusCode).toBe(200)
+    })
+
+    it('should return 400 if id is invalid', async () => {
+        const { sut } = makeSut()
+
+        // act
+        const httpResponse = await sut.execute({
+            params: { userId: 'invalid_id' },
+        })
+
+        // assert
+        expect(httpResponse.statusCode).toBe(400)
+    })
+
+    it('should return 404 if user is not found', async () => {
+        const { sut, getUserBalanceUseCase } = makeSut()
+
+        jest.spyOn(getUserBalanceUseCase, 'execute').mockReturnValueOnce(null)
+        // act
+        const result = await sut.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(404)
+    })
+    // it('should return 500 if DeleteUseCase throws', async () => {
+    //     const { sut, deleteUserUseCase } = makeSut()
+
+    //     // act
+
+    //     jest.spyOn(deleteUserUseCase, 'execute').mockImplementationOnce(() => {
+    //         throw new Error()
+    //     })
+
+    //     const result = await sut.execute(httpRequest)
+
+    //     // assert
+    //     expect(result.statusCode).toBe(500)
+    // })
+})
