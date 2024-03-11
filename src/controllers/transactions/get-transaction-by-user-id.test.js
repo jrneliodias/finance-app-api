48d1f4d17a67542dedc084e64de45c678e5289af
Nélio <jrneliodias@gmail.com>
2024-03-10 23:51:54 -0300

    test: ensure GetTransactionByUserIdController return 201 when return a user transaction successfully

---
+++ src/controllers/transactions/get-transaction-by-user-id.test.js
@@ -0,0 +1,46 @@
+import { faker } from '@faker-js/faker'
+import { GetTransactionByUserIdController } from './get-transaction-by-user-id.js'
+
+describe('GetTransactionByUserIdController', () => {
+    class GetTransactionByUserIdUseCaseStub {
+        async execute() {
+            return {
+                id: faker.string.uuid(),
+                user_id: faker.string.uuid(),
+                name: faker.finance.transactionType(),
+                date: faker.date.anytime().toISOString(),
+                amount: Number(faker.finance.amount()),
+                type: faker.helpers.arrayElement([
+                    'EXPENSE',
+                    'INVESTMENT',
+                    'EARNING',
+                ]),
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const getTransactionByUserIdUseCase =
+            new GetTransactionByUserIdUseCaseStub()
+        const sut = new GetTransactionByUserIdController(
+            getTransactionByUserIdUseCase,
+        )
+        return { getTransactionByUserIdUseCase, sut }
+    }
+
+    const httpRequest = {
+        query: {
+            user_id: faker.string.uuid(),
+        },
+    }
+
+    it('should return 200 when find a transaction by user is successfully', async () => {
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(200)
+    })
+})
