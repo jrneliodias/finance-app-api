66db9c8a108f0a849500571c80ed0ab330a008f4
Nélio <jrneliodias@gmail.com>
2024-05-10 16:04:05 -0300

    test: ensure DeleteUserUseCase delete a user

---
+++ src/use-cases/users/delete-user.test.js
@@ -0,0 +1,38 @@
+import { faker } from '@faker-js/faker'
+import { DeleteUserUseCase } from './delete-user'
+
+describe('DeleteUserUseCase', () => {
+    const user = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 7 }),
+    }
+    class DeleteUserrepositoryStub {
+        async execute(userId) {
+            return { ...user, id: userId }
+        }
+    }
+    const makeSut = () => {
+        const deleteUserRepositoryStub = new DeleteUserrepositoryStub()
+        const sut = new DeleteUserUseCase(deleteUserRepositoryStub)
+
+        return {
+            sut,
+            deleteUserRepositoryStub,
+        }
+    }
+
+    it('should sucessfully delete an user', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const deletedUser = await sut.execute(user.id)
+
+        // assert
+        expect(deletedUser).toBeTruthy()
+        expect(deletedUser).toEqual(user)
+    })
+})
