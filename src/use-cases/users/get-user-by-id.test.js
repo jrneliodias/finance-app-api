2fa15eff048abaaf8d74b6877c79ae4d7a2ef9d9
Nélio <jrneliodias@gmail.com>
2024-05-11 18:52:00 -0300

    test: ensure that GetUserByIdRepository is call with correct params

---
+++ src/use-cases/users/get-user-by-id.test.js
@@ -0,0 +1,38 @@
+import { faker } from '@faker-js/faker'
+import { GetUserByIdUseCase } from './get-user-by-id'
+
+describe('GetUserByIdUseCase', () => {
+    const user = {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 7 }),
+    }
+    class GetUserByIdRepositoryStub {
+        async execute() {
+            return user
+        }
+    }
+
+    const makeSut = () => {
+        const getUserByIdRepositoryStub = new GetUserByIdRepositoryStub()
+        const sut = new GetUserByIdUseCase(getUserByIdRepositoryStub)
+        return { sut, getUserByIdRepositoryStub }
+    }
+
+    it('should GetUserByIdUseCase return user', async () => {
+        const { sut } = makeSut()
+        const user = await sut.execute(faker.string.uuid())
+        expect(user).toEqual(user)
+    })
+
+    it('should call GetUserByIdRepository with correct params', async () => {
+        const { sut, getUserByIdRepositoryStub } = makeSut()
+        const executeSpy = jest.spyOn(getUserByIdRepositoryStub, 'execute')
+        const userId = faker.string.uuid()
+
+        await sut.execute(userId)
+
+        expect(executeSpy).toHaveBeenCalledWith(userId)
+    })
+})
