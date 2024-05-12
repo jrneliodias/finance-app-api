251a4c0692abafd4a18b229acee2a0305b0afbd0
Nélio <jrneliodias@gmail.com>
2024-05-12 18:55:24 -0300

    test: ensure tha GetTransactionByIdUseCase throws if GetTransactionByUserIdRepository throws

--- src/use-cases/transactions/get-transaction-by-user-id.test.js
+++ src/use-cases/transactions/get-transaction-by-user-id.test.js
@@ -61,4 +61,27 @@ describe('GetTransactionByUserIdUseCase', () => {
         const promise = sut.execute(user.id)
         await expect(promise).rejects.toThrow()
     })
+
+    it('should call GetTransactionByUserIdRepository with correct params', async () => {
+        const { sut, getTransactionByUserIdRepositoryStub } = makeSut()
+        const getTransactionByUserIdRepositorySpy = jest.spyOn(
+            getTransactionByUserIdRepositoryStub,
+            'execute',
+        )
+        await sut.execute(user.id)
+        expect(getTransactionByUserIdRepositorySpy).toHaveBeenCalledWith(
+            user.id,
+        )
+    })
+
+    it('should throw if GetUserByIdRepository throws', async () => {
+        const { sut, getUserByIdRepositoryStub } = makeSut()
+        jest.spyOn(getUserByIdRepositoryStub, 'execute').mockImplementationOnce(
+            () => {
+                throw new Error()
+            },
+        )
+        const promise = sut.execute(user.id)
+        await expect(promise).rejects.toThrow()
+    })
 })
