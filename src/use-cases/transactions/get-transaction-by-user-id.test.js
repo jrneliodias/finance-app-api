7c3bfa8d97793fdfd7cf9c32c51bc643039cba31
Nélio <jrneliodias@gmail.com>
2024-05-12 18:37:14 -0300

    test: ensure tha GetTransactionByIdUseCase returns a Transaction list sucessfully

---
+++ src/use-cases/transactions/get-transaction-by-user-id.test.js
@@ -0,0 +1,44 @@
+import { faker } from '@faker-js/faker'
+import { GetTransactionByUserIdUseCase } from './get-transaction-by-user-id'
+
+describe('GetTransactionByUserIdUseCase', () => {
+    const user = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 7 }),
+    }
+    class GetTransactionByUserIdRepositoryStub {
+        async execute() {
+            return []
+        }
+    }
+    class GetUserByIdRepositoryStub {
+        async execute() {
+            return user
+        }
+    }
+
+    const makeSut = () => {
+        const getTransactionByUserIdRepositoryStub =
+            new GetTransactionByUserIdRepositoryStub()
+        const getUserByIdRepositoryStub = new GetUserByIdRepositoryStub()
+        const sut = new GetTransactionByUserIdUseCase(
+            getTransactionByUserIdRepositoryStub,
+            getUserByIdRepositoryStub,
+        )
+        return {
+            sut,
+            getTransactionByUserIdRepositoryStub,
+            getUserByIdRepositoryStub,
+        }
+    }
+    it('should return an array of transactions', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute(user.id)
+
+        expect(result).toEqual([])
+    })
+})
