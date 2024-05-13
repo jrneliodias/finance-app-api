21d41abef407abf9c52ee9413ad5da6311f78d66
Nélio <jrneliodias@gmail.com>
2024-05-13 11:36:16 -0300

    fix: rename mjs to js for jest files

--- jest-global-setup.js
+++ jest-global-setup.js
@@ -3,7 +3,6 @@ import { execSync } from 'child_process'
 async function init() {
     execSync('docker compose up -d --wait postgres-test')
     execSync('npx prisma db push')
-
 }
 
 export default init
