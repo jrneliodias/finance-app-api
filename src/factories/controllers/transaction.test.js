fce23c385e2d56e52185d1c6e9cb3f8dd82f3bab
Nélio <jrneliodias@gmail.com>
2024-05-15 15:54:23 -0300

    test: ensure that Transaction Factories are return properly

---
+++ src/factories/controllers/transaction.test.js
@@ -0,0 +1,46 @@
+import {
+    CreateTransactionController,
+    DeleteTransactionController,
+    GetTransactionByUserIdController,
+    UpdateTransactionController,
+} from '../../controllers/transactions'
+import {
+    makeCreateTransactionController,
+    makeDeleteTransactionController,
+    makeGetTransactioByUserIdController,
+    makeUpdateTransactionController,
+} from './transaction'
+
+describe('Transactions Factories Controller', () => {
+    it('should return an instance of CreateTransactionController', () => {
+        const factoryCreateTransactionController =
+            makeCreateTransactionController()
+        expect(factoryCreateTransactionController).toBeInstanceOf(
+            CreateTransactionController,
+        )
+    })
+
+    it('should return an instance of GetTransactionByUserIdController', () => {
+        const factoryGetTransactionController =
+            makeGetTransactioByUserIdController()
+        expect(factoryGetTransactionController).toBeInstanceOf(
+            GetTransactionByUserIdController,
+        )
+    })
+
+    it('should return an instance of UpdateTransactionController', () => {
+        const factoryUpdateTransactionController =
+            makeUpdateTransactionController()
+        expect(factoryUpdateTransactionController).toBeInstanceOf(
+            UpdateTransactionController,
+        )
+    })
+
+    it('should return a instance of DeleteTransactionController', () => {
+        const factoryDeleteTransactionController =
+            makeDeleteTransactionController()
+        expect(factoryDeleteTransactionController).toBeInstanceOf(
+            DeleteTransactionController,
+        )
+    })
+})
