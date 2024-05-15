ff4f235de2bb20051945b4533284297e10d26da9
Nélio <jrneliodias@gmail.com>
2024-05-15 13:38:43 -0300

    refactor: improve the delete-transactions error handling

--- src/repositories/postgres/transactions/delete-transaction.js
+++ src/repositories/postgres/transactions/delete-transaction.js
@@ -1,4 +1,6 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 import { prisma } from '../../../../prisma/prisma.js'
+import { TransactionNotFoundError } from '../../../errors/index.js'
 
 export class PostgresDeleteTransactionRepository {
     async execute(transactionId) {
@@ -10,7 +12,13 @@ export class PostgresDeleteTransactionRepository {
             })
             return deletedTransaction
         } catch (error) {
-            return null
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2025') {
+                    //"An operation failed because it depends on one or more records that were required but not found. {cause}"
+                    return new TransactionNotFoundError(transactionId)
+                }
+            }
+            return error
         }
     }
 }
